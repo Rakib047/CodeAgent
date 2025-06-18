f06a13825a79eaef4ac5c4e0171fdaa6c2eb5bb6
Ariful-Islam-Shakil <arif.cse18cuet@gmail.com>
2025-06-18 13:53:54 +0600

    Included Environnment variable for accessing api key

--- src/llm/groq_client.py
+++ src/llm/groq_client.py
@@ -1,14 +1,10 @@
 from groq import Groq
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-def get_groq_client(api_key="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"):
-    key = api_key 
-    if not key:
-        raise ValueError("Missing GROQ API key.")
-    return Groq(api_key=key)
-
-
-
-
-
-#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
+def get_groq_client():
+    api_key = os.getenv("GROQ_API_KEY")
+    if not api_key:
+        raise ValueError("Missing GROQ_API_KEY in environment.")
+    return Groq(api_key=api_key)
