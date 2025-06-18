c09e49593c06f726fb2fd5a12d648f649931a3d5
Ariful-Islam-Shakil <arif.cse18cuet@gmail.com>
2025-06-18 13:55:55 +0600

    implemented a feature thate regenerate code with additional instruction.

--- src/llm/refactor_code_optimizer.py
+++ src/llm/refactor_code_optimizer.py
@@ -1,26 +1,29 @@
-
-
-def generate_test_cases(client, code, instruction):
+def optimize_refactored_code(client, code, instruction):
     messages = [
         {
             "role": "system",
             "content": (
-                "You are an expert Python developer and you have generated a good code."
-                "But I need some modification on your developed code on given instruction"
-                "Please re generate the code with with the given instruction"
-                f"Your previously generated code is\n\n: {code} \n\n"
+                "You are an expert Python developer. "
+                "The user has already received a good refactored version of the code, "
+                "but now wants to modify it based on new instructions. "
+                "Please apply the requested modifications and return the updated, clean Python code only."
             )
         },
         {
             "role": "user",
-            "content": f"Please Modify the code with following instruction:\n\n{instruction}"
+            "content": (
+                f"Instruction for code modification:\n{instruction}\n\n"
+                f"Previously generated code:\n{code}"
+            )
         }
     ]
+    
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=messages,
         temperature=0.3,
         top_p=0.9,
-        max_completion_tokens=2048*2
+        max_tokens=2048 * 3
     )
+    
     return response.choices[0].message.content.strip()
